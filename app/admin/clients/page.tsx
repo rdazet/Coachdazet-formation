dd616597404dda238b07814bf5c8c89d14088133
Raphael Dazet <rdazet@hotmail.com>
2026-06-24 13:33:46 +0100

    fix: dynamic import ssr:false pour éviter crash xlsx côté serveur

--- app/admin/clients/page.tsx
+++ app/admin/clients/page.tsx
@@ -1,12 +1,5 @@
-import dynamic from "next/dynamic";
-
-const ClientsTestPage = dynamic(() => import("./ClientsTestPage"), {
-  ssr: false,
-  loading: () => (
-    <div className="p-6 text-sm text-gray-500">Chargement...</div>
-  ),
-});
+import ClientsLoader from "./ClientsLoader";
 
 export default function Page() {
-  return <ClientsTestPage />;
+  return <ClientsLoader />;
 }
