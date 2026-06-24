c9841ffeaa213c9403b39ce3783d98bc5be0db97
Raphael Dazet <rdazet@hotmail.com>
2026-06-24 13:37:41 +0100

    fix: add ClientsLoader manquant

---
+++ app/admin/clients/ClientsLoader.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const ClientsTestPage = dynamic(() => import("./ClientsTestPage"), {
+  ssr: false,
+  loading: () => (
+    <div className="p-6 text-sm text-gray-500">Chargement...</div>
+  ),
+});
+
+export default function ClientsLoader() {
+  return <ClientsTestPage />;
+}
